3658c0c7d912d041fe0b784d98136f4fdd23bef4
josh <joshua@marinacci.org>
2021-04-27 14:15:05 -0700

    use bounds for drawing chrome change chrome color when window is focused add extra verification on the open_window() arguments send mouse events to window contents if on contents start drag only if inside chrome bounds but not content bounds draw window and child windows separately

--- src/DisplayView.js
+++ src/DisplayView.js
@@ -12,79 +12,11 @@ class Manager {
         this.drag_started = false
         this.focused_window = ""
     }
-
-    redraw(c,canvas) {
-        if(!c) return
-        if(!this.windows_list) return
-        //draw background
-        c.save()
-        c.imageSmoothingEnabled = false
-        c.scale(this.SCALE,this.SCALE)
-        c.fillStyle = 'white'
-        c.fillRect(0,0,canvas.width,canvas.height)
-
-        //for each window
-        this.windows_list.forEach(win => {
-            let chrome = win.chrome
-            //draw chrome around the window
-            c.fillStyle = 'cyan'
-            if(win.id === this.focused_window) {
-                c.fillStyle = 'red'
-            }
-            c.fillRect(chrome.x,chrome.y,chrome.width, chrome.height)
-            //draw bg of window
-            c.fillStyle = 'white'
-            c.fillRect(win.bounds.x,win.bounds.y,win.bounds.width,win.bounds.height)
-            //draw contents of window
-            c.drawImage(win.canvas,win.bounds.x,win.bounds.y)
-        })
-        c.restore()
-    }
     init_windows(windows) {
         this.windows_list = []
         this.windows_map = {}
         Object.values(windows).forEach((win)=> this.open_window(win))
     }
-
-    draw_pixel(msg) {
-        let win = this.findWindow(msg.window)
-        if(!win) return
-        let ctx = win.canvas.getContext('2d')
-        ctx.fillStyle = msg.color
-        ctx.fillRect(msg.x,msg.y,1,1)
-    }
-
-    draw_rect(msg) {
-        let win = this.findWindow(msg.window)
-        if(!win) return
-        let ctx = win.canvas.getContext('2d')
-        ctx.fillStyle = msg.color
-        ctx.fillRect(msg.x,msg.y,msg.width,msg.height)
-    }
-
-    draw_image(msg) {
-        let win = this.findWindow(msg.window)
-        if(!win) return
-
-        //copy pixels to new image data
-        let id = new ImageData(msg.width,msg.height)
-        for(let i=0; i<id.data.length; i++) id.data[i] = msg.pixels[i]
-
-        //draw into temp canvas
-        let can = document.createElement('canvas')
-        can.width = msg.width;
-        can.height = msg.height;
-        let scratch = can.getContext('2d')
-        scratch.putImageData(id,0,0)
-
-        //draw temp canvas to window's back buffer
-        let ctx = win.canvas.getContext('2d')
-        ctx.drawImage(can,msg.x,msg.y)
-    }
-    findWindow(win_id) {
-        return this.windows_map[win_id]
-    }
-
     open_window(win) {
         console.log("new window is",win)
         let keys = ['id','x','y','width','height']
@@ -109,7 +41,27 @@ class Manager {
         this.windows_list.push(w)
         this.windows_map[w.id] = w
     }
+    open_child_window(msg) {
+        let win = msg.window
+        let w = {
+            id:win.id,
+            owner:win.owner,
+            parent:msg.parent,
+            window_type:win.window_type,
+            bounds: new Bounds(win.x,win.y,win.width,win.height),
+            chrome:new Bounds(win.x-2,win.y-2-10,win.width+2+2,win.height+2+10+2),
+        }
+        console.log("new child window",w);
+        w.canvas = document.createElement('canvas')
+        w.canvas.width = w.bounds.width
+        w.canvas.height = w.bounds.height
+        let ctx = w.canvas.getContext('2d')
+        ctx.fillStyle = 'red'
+        ctx.fillRect(0,0,w.canvas.width,w.canvas.height)
+        this.windows_list.push(w)
+        this.windows_map[w.id] = w
 
+    }
     close_window(window) {
         let win = this.findWindow(window)
         if(win) {
@@ -119,6 +71,69 @@ class Manager {
             console.log('new indows list',this.windows_list,this.windows_map)
         }
     }
+    findWindow(win_id) {
+        return this.windows_map[win_id]
+    }
+    redraw(c,canvas) {
+        if(!c) return
+        if(!this.windows_list) return
+        //draw background
+        c.save()
+        c.imageSmoothingEnabled = false
+        c.scale(this.SCALE,this.SCALE)
+        c.fillStyle = 'white'
+        c.fillRect(0,0,canvas.width,canvas.height)
+
+        //for each window
+        this.windows_list.forEach(win => {
+            let chrome = win.chrome
+            //draw chrome around the window
+            if(win.window_type === 'plain') {
+                c.fillStyle = 'cyan'
+                if (win.id === this.focused_window) c.fillStyle = 'red'
+                c.fillRect(chrome.x, chrome.y, chrome.width, chrome.height)
+            }
+            //draw bg of window
+            c.fillStyle = 'white'
+            c.fillRect(win.bounds.x,win.bounds.y,win.bounds.width,win.bounds.height)
+            //draw contents of window
+            c.drawImage(win.canvas,win.bounds.x,win.bounds.y)
+        })
+        c.restore()
+    }
+    draw_pixel(msg) {
+        let win = this.findWindow(msg.window)
+        if(!win) return
+        let ctx = win.canvas.getContext('2d')
+        ctx.fillStyle = msg.color
+        ctx.fillRect(msg.x,msg.y,1,1)
+    }
+    draw_rect(msg) {
+        let win = this.findWindow(msg.window)
+        if(!win) return
+        let ctx = win.canvas.getContext('2d')
+        ctx.fillStyle = msg.color
+        ctx.fillRect(msg.x,msg.y,msg.width,msg.height)
+    }
+    draw_image(msg) {
+        let win = this.findWindow(msg.window)
+        if(!win) return
+
+        //copy pixels to new image data
+        let id = new ImageData(msg.width,msg.height)
+        for(let i=0; i<id.data.length; i++) id.data[i] = msg.pixels[i]
+
+        //draw into temp canvas
+        let can = document.createElement('canvas')
+        can.width = msg.width;
+        can.height = msg.height;
+        let scratch = can.getContext('2d')
+        scratch.putImageData(id,0,0)
+
+        //draw temp canvas to window's back buffer
+        let ctx = win.canvas.getContext('2d')
+        ctx.drawImage(can,msg.x,msg.y)
+    }
 
     mouse_down(e) {
         //if clicked on window or within title bar
@@ -126,18 +141,24 @@ class Manager {
         let cursor = new Point((e.clientX-rect.x)/this.SCALE,(e.clientY-rect.y)/this.SCALE)
         let window = this.windows_list.find(win => win.chrome.contains(cursor))
         if(window) {
-            console.log("selected window",window)
             if(window.window_type === 'menubar') {
                 console.log("it's the menubar, send mouse to it instead")
-                this.send(INPUT.MAKE_MouseDown({x:cursor.x,y:cursor.y,target:window.owner}))
+                this.send(INPUT.MAKE_MouseDown({x:cursor.x,y:cursor.y,app:window.owner,window:window.id}))
                 return
             }
             //if inside windown content
             if(window.bounds.contains(cursor)) {
                 console.log("sending mouse event the window")
-                this.send(INPUT.MAKE_MouseDown({x:cursor.x,y:cursor.y,target:window.owner}))
+                this.send(INPUT.MAKE_MouseDown({x:cursor.x,y:cursor.y,app:window.owner, window:window.id}))
+                if(window.id !== this.focused_window && window.window_type !== 'menubar' && window.window_type !== 'menu') {
+                    this.send(WINDOWS.MAKE_SetFocusedWindow({window:window.id, target:window.owner}))
+                    this.focused_window = window.id
+                }
                 return
             }
+            //skip types that can be dragged or made the focus
+            if(window.window_type === 'menubar') return
+            if(window.window_type === 'menu') return
             this.send(WINDOWS.MAKE_SetFocusedWindow({window:window.id}))
             this.focused_window = window.id
             this.drag_started = true
@@ -145,7 +166,6 @@ class Manager {
             this.drag_offset = new Point(window.bounds.x,window.bounds.y).subtract(cursor)
         }
     }
-
     mouse_move(e) {
         if(!this.drag_started) return
         let rect = e.target.getBoundingClientRect();
@@ -157,7 +177,6 @@ class Manager {
         window.chrome.x = window.bounds.x-2
         window.chrome.y = window.bounds.y-2-10
     }
-
     mouse_up(e) {
         this.drag_started = false
         this.drag_window_id = ""
@@ -166,7 +185,6 @@ class Manager {
     setConnection(connection) {
         this.connection = connection
     }
-
     send(msg) {
         if(this.connection) this.connection.send(msg)
     }
@@ -210,6 +228,10 @@ export function DisplayView({connection}) {
             if (msg.type === WINDOWS.TYPE_WindowOpenDisplay) {
                 manager.open_window(msg.window)
                 return redraw()
+            }
+            if (msg.type === WINDOWS.TYPE_create_child_window_display) {
+                console.log("opening a child window",msg)
+                manager.open_child_window(msg)
             }
             //skip
             if(msg.type === WINDOWS.TYPE_WindowOpen) return
