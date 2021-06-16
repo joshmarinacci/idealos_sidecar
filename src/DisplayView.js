63a8df5ea5df8279156e755914e4fe65bf823cb8
josh <joshua@marinacci.org>
2021-06-15 19:27:54 -0700

    improve performance by using grouped messages

--- src/DisplayView.js
+++ src/DisplayView.js
@@ -28,7 +28,8 @@ export function DisplayView({connection,manager,tracker}) {
     const [zoom,set_zoom] = useState(1)
     manager.set_scale(Math.pow(2,zoom))
 
-    function redraw() {
+    function redraw(collect) {
+        if(collect) return
         if(canvas.current) {
             manager.redraw(canvas.current.getContext('2d'),canvas.current,settings)
         }
@@ -76,57 +77,69 @@ export function DisplayView({connection,manager,tracker}) {
         }
     }
 
+    function dispatch_event(msg,collect) {
+        if(msg.type === WINDOWS.TYPE_window_list) {
+            // if(!Array.isArray(msg.windows)) msg.windows = [msg.windows]
+            manager.init_windows(Object.values(msg.windows))
+            Object.values(msg.windows).forEach(win => {
+                connection.send(WINDOWS.MAKE_window_refresh_request({
+                    target:win.owner,
+                    window:win.id,
+                }))
+            })
+            return redraw(collect)
+        }
+        if(msg.type === 'request-font-response') {
+            manager.font_received(msg)
+        }
+        if(msg.type === WINDOWS.TYPE_WindowOpenDisplay) {
+            manager.open_window(msg.window)
+            return redraw(collect)
+        }
+        if(msg.type === WINDOWS.TYPE_create_child_window_display) {
+            manager.open_child_window(msg)
+            return redraw(collect)
+        }
+        if(msg.type === WINDOWS.TYPE_close_child_window_display) {
+            manager.close_window(msg.window)
+            return redraw(collect)
+        }
+        //skip
+        if(msg.type === WINDOWS.TYPE_WindowOpen) return
+        if(msg.type === GRAPHICS.TYPE_DrawPixel) {
+            manager.draw_pixel(msg)
+            return redraw(collect)
+        }
+        if(msg.type === GRAPHICS.TYPE_DrawRect) {
+            tracker.draw_rect(msg)
+            manager.draw_rect(msg)
+            return redraw(collect)
+        }
+        if(msg.type === GRAPHICS.TYPE_DrawImage) {
+            manager.draw_image(msg)
+            return redraw(collect)
+        }
+        if(msg.type === WINDOWS.TYPE_window_close_response) {
+            manager.close_window(msg.window)
+            return redraw(collect)
+        }
+        if(msg.type === 'group-message') {
+            if (msg.category === 'graphics') {
+                console.time("drawing")
+                msg.messages.forEach(msg2 => {
+                    msg2.app = msg.app
+                    msg2.trigger = msg.trigger
+                    dispatch_event(msg2,true)
+                })
+                console.timeEnd("drawing")
+                return redraw()
+            }
+        }
+        console.log("unhandled message",msg)
+    }
     useEffect(() => {
         connection.on("message", (msg) => {
-            if (msg.type === WINDOWS.TYPE_window_list) {
-                // if(!Array.isArray(msg.windows)) msg.windows = [msg.windows]
-                manager.init_windows(Object.values(msg.windows))
-                Object.values(msg.windows).forEach(win => {
-                    connection.send(WINDOWS.MAKE_window_refresh_request({
-                        target:win.owner,
-                        window:win.id,
-                    }))
-                })
-                return redraw()
-            }
-            if(msg.type === 'request-font-response') {
-                console.log("Manager is",manager)
-                manager.font_received(msg)
-            }
-            if (msg.type === WINDOWS.TYPE_WindowOpenDisplay) {
-                manager.open_window(msg.window)
-                return redraw()
-            }
-            if (msg.type === WINDOWS.TYPE_create_child_window_display) {
-                manager.open_child_window(msg)
-                return redraw()
-            }
-            if (msg.type === WINDOWS.TYPE_close_child_window_display) {
-                manager.close_window(msg.window)
-                return redraw()
-            }
-            //skip
-            if(msg.type === WINDOWS.TYPE_WindowOpen) return
-            if(msg.type === GRAPHICS.TYPE_DrawPixel) {
-                manager.draw_pixel(msg)
-                if(canvas.current) manager.redraw(canvas.current.getContext('2d'),canvas.current)
-                return redraw()
-            }
-            if(msg.type === GRAPHICS.TYPE_DrawRect) {
-                tracker.draw_rect(msg)
-                manager.draw_rect(msg)
-                if(canvas.current) manager.redraw(canvas.current.getContext('2d'),canvas.current)
-                return redraw()
-            }
-            if(msg.type === GRAPHICS.TYPE_DrawImage) {
-                manager.draw_image(msg)
-                return redraw()
-            }
-            if(msg.type === WINDOWS.TYPE_window_close_response) {
-                manager.close_window(msg.window)
-                return redraw()
-            }
-            console.log("got message",msg)
+            dispatch_event(msg,false)
         })
     }, [connection])
 
